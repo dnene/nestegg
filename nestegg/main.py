903a1391df8fc592e6010e109d689f5d1e419742
Dhananjay Nene <dhananjay.nene@gmail.com>
2013-08-27 05:05:20 +0530

    Removed redundant top level config node

--- nestegg/main.py
+++ nestegg/main.py
@@ -17,18 +17,14 @@ logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
 class NesteggException(Exception): pass
+class Generic(object): pass
 
 def lookup_config_file(filename):
-    try :
-        return next(filter(lambda f: opath.exists(f), 
-                [opath.join(opath.expanduser('~'), filename),
-                 opath.join(opath.dirname(opath.abspath(sys.argv[0])), 
-                              '../etc/{}'.format(filename)),
-                 '/etc/{}'.format(filename) ]))
-    except StopIteration :
-        return None
+    for f in [opath.join(opath.expanduser('~'), filename),
+              '/etc/{}'.format(filename) ] :
+        if opath.exists(f) : return f
+    raise NesteggException("Cannot find config file {}".format(filename))
 
-class Generic: pass
 def get_config(data) :
     if isinstance(data, dict) :
         config = Generic()
@@ -41,8 +37,8 @@ def get_config(data) :
 
 def scan_packages(config) :
     packages = {}
-    for dirname in os.listdir(config.nestegg.pypi_dir) :
-        if opath.isdir(opath.join(config.nestegg.pypi_dir,dirname)) :
+    for dirname in os.listdir(config.pypi_dir) :
+        if opath.isdir(opath.join(config.pypi_dir,dirname)) :
             packages[dirname.lower()] = dirname
     config.runtime.packages = packages
 
@@ -53,26 +49,23 @@ def get_app():
         "index_url": "https://pypi.python.org/simple",
         "port": 7654,  "fresh": "0",
     }
-    ini_file = lookup_config_file("nestegg.ini")
-    if ini_file :
-        logging.config.fileConfig(ini_file)
     config_file = lookup_config_file("nestegg.yml")
     if config_file :
         with open(config_file,"r") as in_config :
             config = get_config(load(in_config))
         for key, value in default_config.items() :
-            if not hasattr(config.nestegg,key): 
-                setattr(config.nestegg,key,value)
+            if not hasattr(config,key): 
+                setattr(config,key,value)
     else :
         config = get_config(default_config)
-    neconfig = config.nestegg        
-    neconfig.pypi_dir = os.path.join(neconfig.nestegg_dir,"pypi")
-    neconfig.checkout_dir = os.path.join(neconfig.nestegg_dir,"checkout")
-    neconfig.source_dir = os.path.join(neconfig.nestegg_dir,"source_builds")
-    neconfig.archives_dir =os.path.join(neconfig.nestegg_dir,"archived_builds")
-    neconfig.private_packages = set()
+
+    config.pypi_dir = os.path.join(config.nestegg_dir,"pypi")
+    config.checkout_dir = os.path.join(config.nestegg_dir,"checkout")
+    config.source_dir = os.path.join(config.nestegg_dir,"source_builds")
+    config.archives_dir =os.path.join(config.nestegg_dir,"archived_builds")
+    config.private_packages = set()
     config.runtime = Generic()
-    os.makedirs(neconfig.pypi_dir,0o755, exist_ok=True)
+    os.makedirs(config.pypi_dir,0o755, exist_ok=True)
     scan_packages(config)
     app.config['ctx'] = config
     return app
@@ -89,26 +82,26 @@ class NesteggPackageIndex(PackageIndex):
 
 
 def source_build_dir(config, pkg_name) : 
-    return opath.join(config.nestegg.source_dir, pkg_name)
+    return opath.join(config.source_dir, pkg_name)
 
 def archives_dir(config, pkg_name) : 
-    return opath.join(config.nestegg.archives_dir, pkg_name)
+    return opath.join(config.archives_dir, pkg_name)
 
 def source_build_file(config, pkg_name, file_name) : 
-    return opath.join(config.nestegg.source_dir, pkg_name, file_name)
+    return opath.join(config.source_dir, pkg_name, file_name)
 
 def check_source_builds(config):
     cwd = os.getcwd()
-    os.makedirs(config.nestegg.checkout_dir,exist_ok=True)
-    for pkg in  config.nestegg.source_builds :
+    os.makedirs(config.checkout_dir,exist_ok=True)
+    for pkg in  config.source_builds :
         if not hasattr(pkg, "private") : setattr(pkg, "private", False)
-        elif pkg.private : config.nestegg.private_packages.add(pkg.name)
+        elif pkg.private : config.private_packages.add(pkg.name)
         if any(not opath.exists(source_build_file(config,pkg.name, v.dist_file)) 
                     for v in pkg.versions) :
             os.makedirs(source_build_dir(config,pkg.name), exist_ok=True)
-            pkg_co_dir=opath.join(config.nestegg.checkout_dir,pkg.name)
+            pkg_co_dir=opath.join(config.checkout_dir,pkg.name)
             if opath.exists(pkg_co_dir): shutil.rmtree(pkg_co_dir)
-            cd(config.nestegg.checkout_dir)
+            cd(config.checkout_dir)
             call([pkg.repo_type, "clone", pkg.repo_url, pkg.name]) 
             cd(pkg_co_dir)
             for ver in pkg.versions :
@@ -131,7 +124,7 @@ def file_md5(path) :
 def get_pkg_html(config, pkg_path, pkg_idx, pkg_name, pindex):
     os.makedirs(pkg_path, exist_ok=True)
     html_file = opath.join(pkg_path, "index.html")
-    if pkg_name in config.nestegg.private_packages or not pkg_idx[pkg_name] :
+    if pkg_name in config.private_packages or not pkg_idx[pkg_name] :
       versions = {}
     else :
         versions = dict((egg_info(d.location) for d in pkg_idx[pkg_name]))
@@ -158,20 +151,20 @@ def get_pkg_html(config, pkg_path, pkg_idx, pkg_name, pindex):
 @route('/simple/')
 def get_root():
     config, pkg_idx = request.app.config['ctx'], request.app.config['pkg_idx']
-    if not opath.exists(opath.join(config.nestegg.pypi_dir,"index.html")):
+    if not opath.exists(opath.join(config.pypi_dir,"index.html")):
         log.debug("Updating base index")
         pkg_idx.scan_all()
-        with open(opath.join(config.nestegg.pypi_dir,"index.html"), "w") as h :
+        with open(opath.join(config.pypi_dir,"index.html"), "w") as h :
             h.write(app.config['views'].gindex.render(
                 pkgs=sorted(pkg_idx.package_pages)))
-    return static_file("index.html", root=config.nestegg.pypi_dir)    
+    return static_file("index.html", root=config.pypi_dir)    
 
 def is_valid_package(config, pkg_idx, pkg_name) :
-    if pkg_name in config.nestegg.private_packages : return True
+    if pkg_name in config.private_packages : return True
     if opath.exists(archives_dir(config, pkg_name)) : return True
     if opath.exists(source_build_dir(config, pkg_name)) :
         if not pkg_idx[pkg_name] :
-            config.nestegg.private_packages.add(pkg_name)
+            config.private_packages.add(pkg_name)
         return True
     return True if pkg_idx[pkg_name] else False
 
@@ -195,9 +188,9 @@ def get_package(pkg_name):
     real_pkg_name = get_real_mixed_case_name(config,pkg_name)
     if real_pkg_name != pkg_name :
         redirect('/simple/{}/'.format(real_pkg_name))
-    if pkg_name not in config.nestegg.private_packages :
+    if pkg_name not in config.private_packages :
         pkg_idx.find_packages(Requirement.parse(pkg_name))
-    pkg_path = opath.join(config.nestegg.pypi_dir, pkg_name)
+    pkg_path = opath.join(config.pypi_dir, pkg_name)
     if is_valid_package(config, pkg_idx, pkg_name) :
         root, html = get_pkg_html(config, pkg_path, pkg_idx, pkg_name,
                 request.app.config['views'].pindex)
@@ -210,7 +203,7 @@ def get_package(pkg_name):
 def get_egg(pkg_name, egg_name):
     config, pkg_idx = request.app.config['ctx'], request.app.config['pkg_idx']
     log.debug("Package: {} egg:{}".format(pkg_name, egg_name))
-    pkg_dir = opath.join(config.nestegg.pypi_dir, pkg_name)
+    pkg_dir = opath.join(config.pypi_dir, pkg_name)
     if not egg_name.startswith(pkg_name) :
         egg_name="{}-{}.".format(pkg_name,egg_name)
         if opath.exists(pkg_dir) :
@@ -218,7 +211,7 @@ def get_egg(pkg_name, egg_name):
                 if fname.startswith(egg_name) and fname != egg_name :
                     egg_name = fname
                     break
-    fpath = opath.join(config.nestegg.pypi_dir, pkg_name, egg_name)
+    fpath = opath.join(config.pypi_dir, pkg_name, egg_name)
     if not opath.exists(fpath) :
         pkg_idx.find_packages(Requirement.parse(pkg_name))
         for dist in pkg_idx[pkg_name] :
@@ -237,7 +230,7 @@ def get_egg(pkg_name, egg_name):
 def main() :
     app = get_app()  
     config = app.config['ctx']
-    pkg_idx = NesteggPackageIndex(config.nestegg.fresh, config.nestegg.index_url)
+    pkg_idx = NesteggPackageIndex(config.fresh, config.index_url)
     app.config['pkg_idx'] = pkg_idx
     check_source_builds(app.config["ctx"])
     app.config['views'] = Generic()
@@ -245,7 +238,7 @@ def main() :
         SimpleTemplate(resource_string('nestegg','views/package.tpl'))
     app.config['views'].gindex = \
         SimpleTemplate(resource_string('nestegg','views/all_packages.tpl'))
-    run(app=app, host='0.0.0.0', port = config.nestegg.port)
+    run(app=app, host='0.0.0.0', port = config.port)
 
 if __name__ == "__main__" :
     main()
